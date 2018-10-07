eeade244f46647552339269a8a809901a28bcc44
aakashrajput <aakashsingh1999@gmail.com>
2018-10-07 19:38:17 +0530

    created footer.php

---
+++ routes/footer.php
@@ -0,0 +1,262 @@
+<div class="newslatter">
+    <div class="container">
+        <h3>Sign Up for Our <em>Newsletter</em></h3>
+        <p>Subscribe now to get notified about exclusive offers<br> from The .... every week!</p>
+        <form class="navbar-form" role="search">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Your email address" name="q">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">Sign Up</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<!-- /. End of news latter -->
+<div class="insta-content">
+    <div class="insta-link"><a href="index.html#" rel="me" target="_blank" class="">Follow Me!</a></div>
+    <div id="ri-grid" class="ri-grid ri-grid-size-2">
+        <img class="ri-loading-image" src="assets/img/loading.gif" alt=""/>
+        <ul>
+            <li><a href="index.html#"><img src="assets/img/instagram/01.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/02.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/03.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/04.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/05.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/06.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/07.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/08.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/09.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/10.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/11.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/12.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/13.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/14.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/15.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/16.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/17.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/18.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/19.jpg" alt=""/></a></li>
+            <li><a href="index.html#"><img src="assets/img/instagram/20.jpg" alt=""/></a></li>
+        </ul>
+    </div>
+</div>
+<!-- /. End of instagram -->
+<footer class="footer-black">
+    <div class="main-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-md-4">
+                    <div class="footer-box">
+                        <div class="footer-logo">
+                            <img src="assets/img/logo-big1.png" class="img-responsive" alt="">
+                        </div>
+                        <p>It is a long established fact that a reader<br>
+                            will be distracted by the readable content of<br>
+                            a page when looking at its layout. The point<br>
+                            of using Lorem Ipsum</p>
+                        <div class="textwidget"><p>457 BIgBlue Street, Suite 4A<br>
+                                New York, NY 10013<br>
+                                <span>(315) 5512-2579</span><br>
+                                <!--<a href="mailto:info@example.com">info@example.com</a>-->
+                            </p>
+                        </div>
+                        <p>Contact us on <a href="../cdn-cgi/l/email-protection.html#30595e565f705548515d405c551e535f5d"><strong><span class="__cf_email__" data-cfemail="11787f777e517469707c617d743f727e7c">[email&#160;protected]</span></strong></a></p>
+                    </div>
+                </div>
+                <div class="hidden-sm col-md-3">
+                    <div class="footer-box">
+                        <h3 class="widget-title title-white">Twitter</h3>
+                        <ul class="twitter-widget">
+                            <li>
+                                <div class="icon"><i class="ti-twitter"></i></div>
+                                <div class="tweet-text">
+                                    So proud to have become an <a target="_blank" href="index.html#">@envato</a> Power Elite author. Drop our #ThemeForest: <a target="_blank" href="index.html#">https://t.co/jGyLLggygN</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon"><i class="ti-twitter"></i></div>
+                                <div class="tweet-text">
+                                    So proud to have become an <a target="_blank" href="index.html#">@envato</a> Power Elite author. Drop out our #ThemeForest: <a target="_blank" href="index.html#">https://t.co/jGyLLggygN</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-md-2">
+                    <div class="footer-box">
+                        <h3 class="widget-title title-white">Need help</h3>
+                        <ul class="footer-cat">
+                            <li><a href="index.html#">Our Community</a></li>
+                            <li><a href="index.html#">See Our Portfolio</a></li>
+                            <li><a href="index.html#">Story About Us</a></li>
+                            <li><a href="index.html#">Keep in Touch</a></li>
+                            <li><a href="index.html#">Purchase Our Products</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-5 col-md-3">
+                    <div class="footer-box">
+                        <h3 class="widget-title title-white">Latest Post</h3>
+                        <div class="media latest_post">
+                            <a class="media-left" href="index.html">
+                                <img src="assets/img/100x70-1.jpg" class="media-object" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h6 class="media-heading"><a href="index.html#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
+                                <div class="entry-meta">
+                                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.latest post -->
+                        <div class="media latest_post">
+                            <a class="media-left" href="index.html">
+                                <img src="assets/img/100x70-2.jpg" class="media-object" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h6 class="media-heading"><a href="index.html#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
+                                <div class="entry-meta">
+                                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.latest post -->
+                        <div class="media latest_post">
+                            <a class="media-left" href="index.html">
+                                <img src="assets/img/100x70-3.jpg" class="media-object" alt="">
+                            </a>
+                            <div class="media-body">
+                                <h6 class="media-heading"><a href="index.html#">The <em>Best Street-Style</em> Pics Copenhagen</a></h6>
+                                <div class="entry-meta">
+                                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.latest post -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sub-footer">
+        Copyright © 2017 by Bdtask. All Rights Reserved.
+    </div>
+</footer>
+<!-- /.End of footer -->
+</div>
+<!-- /.End of main content -->
+<div class="modal fade user-modal" id="user-modal">
+<div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+        <div class="modal-body">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="index.html#login" data-toggle="tab">Log in</a></li>
+                <li><a href="index.html#register" data-toggle="tab">Register</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane fade in active" id="login">
+                    <div class="form-content text-center">
+                        <h2>Sign In</h2>
+                        <p>Chose One of the Following Methods.</p>
+                        <div class="social-btn">
+                            <a href="index.html#" class="btn btn-fb"><i class="fa fa-facebook"></i>With Facebook</a>
+                            <a href="index.html#" class="btn btn-plush"><i class="fa fa-google-plus"></i>With Google +</a>
+                        </div>
+                        <div class="ui horizontal divider">Or </div>
+                        <p>Sign in Using Your Email Address</p>
+                        <div class="form-group">
+                            <input class="form-control" name="f_name" id="f_name2" placeholder="Username or email" type="text">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" name="pass" id="pass" placeholder="Password" type="text">
+                        </div>
+                        <div class="block-content">
+
+                            <div class="checkbox checkbox-danger">
+                                <input id="checkbox8" type="checkbox">
+                                <label for="checkbox8">
+                                    Remember me
+                                </label>
+                            </div>
+                            <a href="index.html#" class="forgot">Forgot Password?</a>
+                        </div>
+                        <a href="index.html#" class="btn link-btn btn-block btn-rounded">Login &#8702;</a>
+                        <div class="">Don't have an account? <a href="index.html#">Sign up Now</a></div>
+                    </div>
+                    <!-- /.End of Login -->
+                </div>
+                <div class="tab-pane fade" id="register">
+                    <div class="form-content">
+                        <h2 class="text-center">Sign Up For Free</h2>
+                        <div class="form-group">
+                            <input class="form-control" name="name" id="name" placeholder="Your Name" type="text">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" name="email" id="email" placeholder="Your Email Address" type="text">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" name="c_name" id="c_name" placeholder="Your Company Name" type="text">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" name="pass2" id="pass2" placeholder="Enter Your Password" type="text">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" name="r_pass" id="r_pass" placeholder="Retype Your Password" type="text">
+                        </div>
+                        <div class="block-content">
+                            <div><i class="fa fa-shield"></i><span>Your Password at Global Crypto are encrypted and Secured</span></div>
+                        </div>
+                        <a href="index.html#" class="btn link-btn btn-block btn-rounded">Sign Up &#8702;</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- /.End of Sign up  Sing in -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/animsition.min.js"></script>
+<script src="assets/js/bootsnav.js"></script>
+<script src="assets/js/macy.js"></script>
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="assets/js/ResizeSensor.min.js"></script>
+<script src="assets/js/theia-sticky-sidebar.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/owl-carousel/owl.carousel.min.js"></script>
+<script src="assets/js/modernizr.custom.js"></script>
+<script src="assets/js/jquery.gridrotator.min.js"></script>
+<script src="assets/js/parallax-background.min.js"></script>
+<script src="assets/js/jquery.simpleSocialShare.min.js"></script>
+<script src="assets/js/jquery.fluidbox.min.js"></script>
+<script src="assets/js/retina.min.js"></script>
+<script src="assets/js/jquery.shuffle.min.js"></script>
+<script src="assets/js/readingTime.min.js"></script>
+<script src="assets/js/custom.js"></script>
+<script>
+$(document).ready(function () {
+    'use strict';
+    //Grid content
+    var masonry = new Macy({
+        container: '.grid-content',
+        trueOrder: false,
+        waitForImages: false,
+        useOwnImageLoader: false,
+        debug: true,
+        mobileFirst: true,
+        columns: 1,
+        margin: 30,
+        breakAt: {
+            1200: 2,
+            992: 2,
+            768: 2,
+            480: 2
+        }
+    });
+});
+</script>
+</body>
+</html>
