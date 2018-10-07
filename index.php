427d87c67766e662ec8ef8b5db9c04a2c1c7d7e6
aakashrajput <aakashsingh1999@gmail.com>
2018-10-07 19:42:50 +0530

    fixed fontsand created new route

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <?php
 include ("./routes/header.php");
+include ("./routes/trending.php")
 ?>
-            <div class="container">
-                <div class="newstricker_inner">
-                    <div class="trending"><strong>Trending</strong> Now</div>
-                    <div id="newsTicker" class="owl-carousel owl-theme">
-                        <div class="item">
-                            <a href="index.html#">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-                        </div>
-                        <div class="item">
-                            <a href="index.html#">It is a long established fact that a reader will be distracted by the readable.</a>
-                        </div>
-                        <div class="item">
-                            <a href="index.html#">Contrary to popular belief, Lorem Ipsum is not simply random text.</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
             <!--  /. End of newstricker -->
             <div class="news-masonry">
                 <div class="container">
