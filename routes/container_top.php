8402c8108bfa6327290e7dfccafef031d02bceaa
aakashrajput <aakashsingh1999@gmail.com>
2018-11-18 00:11:18 +0530

    added slider

--- routes/container_top.php
+++ routes/container_top.php
@@ -24,10 +24,10 @@
                 <div class="mas-item mas-big">
                     <a href="index.html#"><figure><img src="assets/img/masonry/568x450-01.jpg" class="img-responsive" alt=""></figure></a>
                     <div class="mas-text">
-                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories ?></a></div>
-                        <h2 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader will be distracted by the readable content</a></h2>
+                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories; ?></a></div>
+                        <h2 class="mas-title"><a href="index.html#"><?php echo $container_title; ?></a></h2>
                         <div class="mas-details">
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+                            <p><?php echo substr($container_data,0,150); ?></p>
                             <a href="index.html#" class="read-more">Read More &#8702;</a>
                         </div>
                     </div>
@@ -39,132 +39,124 @@
                 <div class="row mas-m">
                     <div class="col-xs-6 col-sm-6 mas-p">
                         <div class="masonry-slide1 owl-carousel owl-theme">
+                          <?php
+                              $container_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY id DESC LIMIT 2";
+                              $container_run = mysqli_query($link, $container_query);
+                              $check = 0;
+                                while($container_row = mysqli_fetch_array($container_run)) {
+                                  $container_id = $container_row['id'];
+                                  $container_categories = $container_row['categories'];
+                                  $container_image = $container_row['image'];
+                                  $container_title = $container_row['title'];
+                                  $container_data = $container_row['post_data'];
+                            ?>
                             <div class="item mas-m-b">
                                 <div class="mas-item masonry-sm">
                                     <a href="index.html#"><figure><img src="assets/img/masonry/282x223-01.jpg" class="img-responsive" alt=""></figure></a>
                                     <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
+                                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories; ?></a></div>
+                                        <h4 class="mas-title"><a href="index.html#"><?php echo $container_title; ?></a></h4>
                                         <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
+                                            <p><?php echo substr($container_data,0,50); ?></p>
                                             <a href="index.html#" class="read-more">Read More &#8702;</a>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- /.End of masonry item -->
                             </div>
-                            <div class="item mas-m-b">
-                                <div class="mas-item masonry-sm">
-                                    <a href="index.html#"><figure><img src="assets/img/masonry/282x223-02.jpg" class="img-responsive" alt=""></figure></a>
-                                    <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
-                                        <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
-                                            <a href="index.html#" class="read-more">Read More &#8702;</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.End of masonry item -->
-                            </div>
+                          <?php } ?>
                         </div>
                     </div>
                     <div class="col-xs-6 col-sm-6 mas-p">
                         <div class="masonry-slide2 owl-carousel owl-theme">
-                            <div class="item">
-                                <div class="mas-item masonry-sm">
-                                    <a href="index.html#"><figure><img src="assets/img/masonry/282x223-03.jpg" class="img-responsive" alt=""></figure></a>
-                                    <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
-                                        <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
-                                            <a href="index.html#" class="read-more">Read More &#8702;</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.End of masonry item -->
-                            </div>
+                          <?php
+                              $container_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY id DESC LIMIT 2";
+                              $container_run = mysqli_query($link, $container_query);
+                              $check = 0;
+                                while($container_row = mysqli_fetch_array($container_run)) {
+                                  $container_id = $container_row['id'];
+                                  $container_categories = $container_row['categories'];
+                                  $container_image = $container_row['image'];
+                                  $container_title = $container_row['title'];
+                                  $container_data = $container_row['post_data'];
+                            ?>
                             <div class="item">
                                 <div class="mas-item masonry-sm">
                                     <a href="index.html#"><figure><img src="assets/img/masonry/282x223-04.jpg" class="img-responsive" alt=""></figure></a>
                                     <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
+                                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories; ?></a></div>
+                                        <h4 class="mas-title"><a href="index.html#"><?php echo $container_title; ?></a></h4>
                                         <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
+                                            <p><?php echo substr($container_data,0,50); ?></p>
                                             <a href="index.html#" class="read-more">Read More &#8702;</a>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- /.End of masonry item -->
                             </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
                 <div class="hidden-xs row mas-m">
                     <div class="col-sm-6 mas-p">
                         <div class="masonry-slide3 owl-carousel owl-theme">
+                          <?php
+                              $container_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY id DESC LIMIT 2";
+                              $container_run = mysqli_query($link, $container_query);
+                              $check = 0;
+                                while($container_row = mysqli_fetch_array($container_run)) {
+                                  $container_id = $container_row['id'];
+                                  $container_categories = $container_row['categories'];
+                                  $container_image = $container_row['image'];
+                                  $container_title = $container_row['title'];
+                                  $container_data = $container_row['post_data'];
+                            ?>
                             <div class="item">
                                 <div class="mas-item masonry-sm">
                                     <a href="index.html#"><figure><img src="assets/img/masonry/282x223-05.jpg" class="img-responsive" alt=""></figure></a>
                                     <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
+                                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories; ?></a></div>
+                                        <h4 class="mas-title"><a href="index.html#"><?php echo $container_title; ?></a></h4>
                                         <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
+                                            <p><?php echo substr($container_data,0,50); ?></p>
                                             <a href="index.html#" class="read-more">Read More &#8702;</a>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- /.End of masonry item -->
                             </div>
-                            <div class="item">
-                                <div class="mas-item masonry-sm">
-                                    <a href="index.html#"><figure><img src="assets/img/masonry/282x223-06.jpg" class="img-responsive" alt=""></figure></a>
-                                    <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
-                                        <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
-                                            <a href="index.html#" class="read-more">Read More &#8702;</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.End of masonry item -->
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="col-sm-6 mas-p">
                         <div class="masonry-slide4 owl-carousel owl-theme">
+                          <?php
+                              $container_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY id DESC LIMIT 2";
+                              $container_run = mysqli_query($link, $container_query);
+                              $check = 0;
+                                while($container_row = mysqli_fetch_array($container_run)) {
+                                  $container_id = $container_row['id'];
+                                  $container_categories = $container_row['categories'];
+                                  $container_image = $container_row['image'];
+                                  $container_title = $container_row['title'];
+                                  $container_data = $container_row['post_data'];
+                            ?>
                             <div class="item">
                                 <div class="mas-item masonry-sm">
                                     <a href="index.html#"><figure><img src="assets/img/masonry/282x223-07.jpg" class="img-responsive" alt=""></figure></a>
                                     <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
+                                        <div class="post-cat"><a href="index.html#"><?php echo $container_categories; ?></a></div>
+                                        <h4 class="mas-title"><a href="index.html#"><?php echo $container_title; ?></a></h4>
                                         <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
+                                            <p><?php echo substr($container_data,0,50); ?></p>
                                             <a href="index.html#" class="read-more">Read More &#8702;</a>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- /.End of masonry item -->
                             </div>
-                            <div class="item">
-                                <div class="mas-item masonry-sm">
-                                    <a href="index.html#"><figure><img src="assets/img/masonry/282x223-08.jpg" class="img-responsive" alt=""></figure></a>
-                                    <div class="mas-text">
-                                        <div class="post-cat"><a href="index.html#">Fashion</a></div>
-                                        <h4 class="mas-title"><a href="index.html#">It is a long <em>established</em> fact that a reader</a></h4>
-                                        <div class="mas-details">
-                                            <p>There are many variations of passages of Lorem Ipsum available</p>
-                                            <a href="index.html#" class="read-more">Read More &#8702;</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.End of masonry item -->
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
