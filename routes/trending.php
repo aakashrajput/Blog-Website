da1adc349de3fcd54dc04704a123ec90044fe371
aakashrajput <aakashsingh1999@gmail.com>
2018-11-18 00:30:55 +0530

    added trending bar

--- routes/trending.php
+++ routes/trending.php
@@ -2,15 +2,17 @@
     <div class="newstricker_inner">
         <div class="trending"><strong>Trending</strong> Now</div>
         <div id="newsTicker" class="owl-carousel owl-theme">
+          <?php
+      $p_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY views DESC LIMIT 5";
+      $p_run = mysqli_query($link,$p_query);
+      if(mysqli_num_rows($p_run) > 0){
+        while($p_row = mysqli_fetch_array($p_run)){
+          $p_title = $p_row['title'];
+      ?>
             <div class="item">
-                <a href="index.html#">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
+                <a href="index.html#"><?php echo $p_title; ?></a>
             </div>
-            <div class="item">
-                <a href="index.html#">It is a long established fact that a reader will be distracted by the readable.</a>
-            </div>
-            <div class="item">
-                <a href="index.html#">Contrary to popular belief, Lorem Ipsum is not simply random text.</a>
-            </div>
+      <?php } ?>
         </div>
     </div>
 </div>
