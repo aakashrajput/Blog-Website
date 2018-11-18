c41626563649b0c450a2a219e73de479888104ad
aakashrajput <aakashsingh1999@gmail.com>
2018-11-18 13:01:24 +0530

    working on post

--- routes/recent_posts.php
+++ routes/recent_posts.php
@@ -21,16 +21,16 @@
              ?>
     <article class="grid_post grid_post_lg text-center">
         <figure>
-            <a href="posts.php" class="grid_image"><img src="assets/img/825x630-2.jpg" class="img-responsive" alt=""></a>
+            <a href="posts.php?post=<?php echo $id; ?>" class="grid_image"><img src="assets/img/825x630-2.jpg" class="img-responsive" alt=""></a>
             <figcaption>
                 <div class="post-cat"><span>In</span> <a href="index.html#"><?php echo $categories; ?></a></div>
                 <div class="entry-meta">
                     <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00"><?php echo "$month $day, $year " ?></time></span>
                     <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
                 </div>
-                <h3 class="grid_post_title"><a href="posts.php"><?php echo $title; ?></a></h3>
+                <h3 class="grid_post_title"><a href="posts.php?post=<?php echo $id; ?>"><?php echo $title; ?></a></h3>
                 <p><?php echo substr($post_data,0,200); ?></p>
-                <a href="posts.php" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
+                <a href="posts.php?post=<?php echo $id; ?>" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
                 <!-- /.Post button -->
             </figcaption>
         </figure>
@@ -76,13 +76,13 @@
                         <!-- /.End of grid post -->
                         <div class="media meida-md">
                             <div class="media-left">
-                                <a href="posts.php"><img src="assets/img/370x300-1.jpg" class="media-object" alt=""></a>
+                                <a href="posts.php?post=<?php echo $id; ?>"><img src="assets/img/370x300-1.jpg" class="media-object" alt=""></a>
                             </div>
                             <!-- /.Post image -->
                             <div class="media-body">
                                 <div class="post-header">
-                                    <div class="post-cat"><span>In</span> <a href="posts.php"><?php echo $categories; ?></a></div>
-                                    <h3 class="media-heading"><a href="posts.php"><?php echo $title; ?></a></h3>
+                                    <div class="post-cat"><span>In</span> <a href="#"><?php echo $categories; ?></a></div>
+                                    <h3 class="media-heading"><a href="posts.php?post=<?php echo $id; ?>"><?php echo $title; ?></a></h3>
                                     <div class="entry-meta">
                                         <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00"><?php echo "$month $day, $year"; ?></time></span>
                                         <span class="comment-link"><a href="posts.php"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
@@ -91,7 +91,7 @@
                                 </div>
                                 <p><?php echo substr($post_data,0,50); ?></p>
                                 <div class="element-block">
-                                    <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
+                                    <a href="posts.php?post=<?php echo $id; ?>" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
                                     <!-- /.Post button -->
                                     <div class="post_share">
                                         <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
