8ae97a8af081c6a61a88a13d6f4d774ce279d2a7
aakashrajput <aakashsingh1999@gmail.com>
2018-11-18 10:12:52 +0530

    completed recent post

--- routes/recent_posts.php
+++ routes/recent_posts.php
@@ -38,210 +38,59 @@
     <?php
                  }
             ?>
+            <?php
+                $post_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY title DESC LIMIT 5";
+                $post_run = mysqli_query($link, $post_query);
+                $check = 0;
+                  while($row = mysqli_fetch_array($post_run)) {
+                            $id = $row['id'];
+                            $date = getdate ($row['date']);
+                            $day = $date['mday'];
+                            $month = $date['month'];
+                            $year = $date['year'];
+                            $title = $row['title'];
+                            $author = $row['author'];
+                            $author_image = $row['author_image'];
+                            $categories = $row['categories'];
+                            $tags = $row['tags'];
+                            $post_data = $row['post_data'];
+                            $views = $row['views'];
+                            $status = $row['status'];
+                            $image = $row['image'];
+                       ?>
+                        <!-- /.End of grid post -->
+                        <div class="media meida-md">
+                            <div class="media-left">
+                                <a href="index.html#"><img src="assets/img/370x300-1.jpg" class="media-object" alt=""></a>
+                            </div>
+                            <!-- /.Post image -->
+                            <div class="media-body">
+                                <div class="post-header">
+                                    <div class="post-cat"><span>In</span> <a href="index.html#"><?php echo $categories; ?></a></div>
+                                    <h3 class="media-heading"><a href="index.html#"><?php echo $title; ?></a></h3>
+                                    <div class="entry-meta">
+                                        <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00"><?php echo "$month $day, $year"; ?></time></span>
+                                        <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
+                                    </div>
+                                    <!-- /.Post meta -->
+                                </div>
+                                <p><?php echo substr($post_data,0,50); ?></p>
+                                <div class="element-block">
+                                    <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
+                                    <!-- /.Post button -->
+                                    <div class="post_share">
+                                        <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
+                                        <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
+                                        <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
+                                    </div>
+                                    <!-- /.Post social share -->
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                           }
+                        ?>
 
-    <!-- /.End of grid post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-1.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Fashion</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-2.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Lifestyle</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-3.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Travel</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-4.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Fashion</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-5.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Lifestyle</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-6.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Fashion</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
-    <!-- /.End of media post -->
-    <div class="media meida-md">
-        <div class="media-left">
-            <a href="index.html#"><img src="assets/img/370x300-7.jpg" class="media-object" alt=""></a>
-        </div>
-        <!-- /.Post image -->
-        <div class="media-body">
-            <div class="post-header">
-                <div class="post-cat"><span>In</span> <a href="index.html#">Lifestyle</a></div>
-                <h3 class="media-heading"><a href="index.html#">It is a long <em>established fact</em> that a reader will be distracted.</a></h3>
-                <div class="entry-meta">
-                    <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00">Jan 21, 2018</time></span>
-                    <span class="comment-link"><a href="index.html#"><i class="fa fa-comment-o" aria-hidden="true"></i>9 Comments</a></span>
-                </div>
-                <!-- /.Post meta -->
-            </div>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority.</p>
-            <div class="element-block">
-                <a href="index.html#" class="btn link-btn btn-outline btn-rounded">Reading &#8702;</a>
-                <!-- /.Post button -->
-                <div class="post_share">
-                    <a class="smedia facebook fa fa-facebook" href="index.html#"></a>
-                    <a class="smedia twitter fa fa-twitter" href="index.html#"></a>
-                    <a class="smedia googleplus fa fa-google-plus" href="index.html#"></a>
-                </div>
-                <!-- /.Post social share -->
-            </div>
-        </div>
-    </div>
     <!-- /.End of media post -->
     <!--                            <div class="loadmore_block">
                                     <a href="#" class=""> Load More</a>
