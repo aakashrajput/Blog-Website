7e0ad3f34e4545a4db07a43c2a3bd5a12f5816d3
aakashrajput <aakashsingh1999@gmail.com>
2018-11-18 10:28:51 +0530

    added category section

--- routes/category.php
+++ routes/category.php
@@ -3,22 +3,20 @@
         <h3 class="title">Category</h3>
         <span class="title-shape title-shape-dark"></span>
     </div>
+    <?php
+        $cat_query = "SELECT * FROM categories ORDER BY id DESC LIMIT 4";
+        $cat_run = mysqli_query($link, $cat_query);
+          while($row = mysqli_fetch_array($cat_run)) {
+                    $id = $row['id'];
+                    $category = $row['category'];
+    ?>
     <!--  /.End of title -->
     <a class="category" href="index.html">
         <figure><img src="assets/img/category-1.jpg" class="img-responsive" alt=""></figure>
-        <div class="category_name">Fashion</div>
+        <div class="category_name"><?php echo $category; ?></div>
     </a>
     <!-- /.End of category -->
-    <a class="category" href="index.html">
-        <figure><img src="assets/img/category-2.jpg" class="img-responsive" alt=""></figure>
-        <div class="category_name">Travel</div>
-    </a>
-    <!-- /.End of category -->
-    <a class="category" href="index.html">
-        <figure><img src="assets/img/category-3.jpg" class="img-responsive" alt=""></figure>
-        <div class="category_name">Life Style</div>
-    </a>
-    <!-- /.End of category -->
+  <?php } ?>
 </div>
 <!-- /.End of category widget -->
 </aside>
