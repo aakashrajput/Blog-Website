b93c8d0fb00ee44b677081b0c6620f213824f092
aakashrajput <aakashsingh1999@gmail.com>
2018-11-25 17:01:00 +0530

    minor

--- admin/header.php
+++ admin/header.php
@@ -375,7 +375,7 @@ if(isset($_POST['checkboxes'])){
                   <img alt="Image placeholder" src="./assets/img/theme/team-4-800x800.jpg">
                 </span>
                 <div class="media-body ml-2 d-none d-lg-block">
-                  <span class="mb-0 text-sm  font-weight-bold">Jessica Jones</span>
+                  <span class="mb-0 text-sm  font-weight-bold">User</span>
                 </div>
               </div>
             </a>
