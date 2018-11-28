9092c804557edc0003d1fbecca9111dc1c5af691
aakashrajput <aakashsingh1999@gmail.com>
2018-11-28 20:03:45 +0530

    testing article

--- routes/post_data.php
+++ routes/post_data.php
@@ -34,8 +34,8 @@ if(isset($_GET['post_id'])) {
                       <div class="details-body">
                           <div class="post_details stickydetails">
                               <header class="details-header">
-                                  <div class="post-cat"><a href="details-image.html#">Fashion</a><a href="details-image.html#">Travel</a><a href="details-image.html#">Lifestyle</a></div>
-                                  <h2><?php echo $title; ?></h2>
+                                  <div class="post-cat"><a href="details-image.html#"><?php echo ucfirst($categories); ?></a><a href="details-image.html#"><?php echo $tags; ?></a></div>
+                                  <h2><?php echo ucfirst($title); ?></h2>
                                   <div class="element-block">
                                       <div class="entry-meta">
                                           <span class="entry-date"><i class="fa fa-calendar-o" aria-hidden="true"></i><time datetime="2018-01-21T19:00"><?php echo "$month $day, $year"; ?></time></span>
@@ -47,60 +47,13 @@ if(isset($_GET['post_id'])) {
                               <figure>
                                   <img src="assets/img/details-4.jpg" alt="" class="aligncenter img-responsive">
                               </figure>
-                              <h3>It is a long established fact that a reader will be distracted. </h3>
-                              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-                                  text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-                                  survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                              <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-                                  desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                              <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected
-                                  humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need
-                                  to be sure there isn't anything embarrassing hidden in the middle.</p>
+                              <h3>Sub Title </h3>
+                              <p><?php echo substr($post_data, 0, ($spos = strpos($post_data, ' ', $lcount = count($post_data) > 550 ? $lcount : 550)) ? $spos : $lcount );?></p>
                               <a href="assets/img/details-2.jpg" class="fluidbox_img">
                                   <img src="assets/img/details-2.jpg" alt="Image" class="alignright img-responsive">
                               </a>
-                              <p> Oh, you're gonna be in a coma, all right. I care deeply for nature. I care deeply for nature.
-                                  I'm a monster. I don't criticize you! And if you're worried about criticism, sometimes a diet is
-                                  the best defense.</p>
-                              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
-                                  making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more
-                                  obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered
-                                  the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil)
-                                  by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
-                                  "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-                              <blockquote>
-                                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form,
-                                  by injected humour, or randomised words which don't look even slightly believable.
-                              </blockquote>
-                              <h3>There are many variations of passages of Lorem Ipsum available.</h3>
-                              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC,
-                                  making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more
-                                  obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered
-                                  the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil)
-                                  by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
-                                  "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-                              <p>
-                                  <a href="assets/img/details-5.jpg" class="fluidbox_img"><img src="assets/img/details-5.jpg" alt="Image" class="alignleft img-responsive"></a>
-                                  Oh, you're gonna be in a coma, all right. I care deeply for nature. I care deeply for nature.
-                                  I'm a monster. I don't criticize you! And if you're worried about criticism, sometimes a diet is
-                                  the best defense.
-                              </p>
-                              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-                                  The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content
-                                  here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use
-                                  Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                  Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                              <h4>It is a long established fact that a reader will be distracted</h4>
-                              <ul>
-                                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                                  <li>Quisque mollis quam vitae arcu cursus lacinia.</li>
-                                  <li>Praesent condimentum nunc eget ligula rutrum pretium.</li>
-                                  <li>Fusce rhoncus eros et auctor tincidunt.</li>
-                                  <li>Donec eu neque at turpis tempus pretium rhoncus sed neque.</li>
-                                  <li>In nec metus commodo, semper dolor vitae, volutpat lacus.</li>
-                                  <li>Donec maximus dolor in felis ornare, et euismod enim lobortis.</li>
-                              </ul>
-                              <p>Source:&nbsp;https://unsplash.com/</p>
+                              <p><?php echo substr($post_data, 550, ($spos = strpos($post_data, ' ', $lcount = count($post_data) > 850 ? $lcount : 850)) ? $spos : $lcount );
+?></p>
                           </div>
                           <!-- /.End of post details -->
                           <div class="stickyshare">
